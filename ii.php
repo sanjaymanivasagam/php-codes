21842920aa727a3ce7c826e562bdfadcb103a33c
sanjaymanivasagam <sanjaymanivasagam6@gmail.com>
2026-06-23 11:42:22 +0530

    Add files via upload

---
+++ ii.php
@@ -0,0 +1,47 @@
+<?php
+/*$_str1='hello';
+$_str2='what is your name ';
+$_str3='4';
+echo $_str1;
+echo '<br>';
+echo $_str2;
+echo '<br>';
+echo $_str3;
+echo '<br>';
+define('collage','kgisl iim');
+$name="sanjay";
+$age=24;
+$weight=80;
+$fname="sanjay";
+$lname="mani";
+print "my name is ".$fname,$lname;
+echo " enter your name";
+$name = trim(fgets(STDIN));
+echo "hello ".$name;
+$_str1='hello word';
+strpos($_str1,'word');
+echo strpos($_str1,'llo');*/
+/*$a='php';
+$a.= ' programming';
+
+echo $a;*/
+$a=6;
+$b=3;
+echo $a&$b;
+echo "<br>";
+echo $a|$b;
+/*echo "<br>";
+echo $a;
+echo "<br>";
+echo $a--;
+echo "<br>";
+echo $a;*/
+/*echo "<br>";
+echo $a-$b;
+echo "<br>";
+echo $a*$b;
+echo "<br>";
+echo $a/$b;
+echo "<br>";
+echo $a%$b;*/
+?>
